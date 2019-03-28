958914656891c5ae51fbaec4aa4ac1532fa4d931
Nicolas Saenz Julienne <nsaenzjulienne@suse.de>
2019-03-28 18:08:02 +0100

    serial: 8250: bcm2835aux: add notifier to follow clock changes
    The serial port's clock is attached to RPi's vpu clock. Which might be
    changed by the cpufreq driver anytime. This adds a cpufreq notifier,
    which stops the serial port before the change, reconfigures it and
    enables it again after the clock changes. Making sure the serial output
    is sane anytime.
    
    Signed-off-by: Nicolas Saenz Julienne <nsaenzjulienne@suse.de>

--- drivers/tty/serial/8250/8250_bcm2835aux.c
+++ drivers/tty/serial/8250/8250_bcm2835aux.c
@@ -9,18 +9,89 @@
  */
 
 #include <linux/clk.h>
-#include <linux/io.h>
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/platform_device.h>
 
 #include "8250.h"
 
+#define UART_BCM2835AUX_CNTL	8
+
 struct bcm2835aux_data {
 	struct uart_8250_port uart;
 	struct clk *clk;
+	struct notifier_block clk_notifier;
 	int line;
 };
+
+static int bcm2835aux_clk_notifier_cb(struct notifier_block *nb,
+				      unsigned long event, void *priv)
+{
+	struct bcm2835aux_data *data = container_of(nb, struct bcm2835aux_data,
+						    clk_notifier);
+	struct uart_8250_port *uart = serial8250_get_port(data->line);
+	struct uart_port *up = &uart->port;
+	unsigned long flags;
+
+	if (up->suspended)
+		return NOTIFY_OK;
+
+	switch (event) {
+	case PRE_RATE_CHANGE:
+		console_lock();
+		serial8250_rpm_get(uart);
+		spin_lock_irqsave(&up->lock, flags);
+		/* Stop UART */
+		serial_out(uart, UART_BCM2835AUX_CNTL, 0);
+		spin_unlock_irqrestore(&up->lock, flags);
+		serial8250_rpm_put(uart);
+
+		return NOTIFY_OK;
+	case POST_RATE_CHANGE:
+	{
+		struct clk_notifier_data *ndata = priv;
+		struct ktermios termios;
+		unsigned int quot, baud;
+
+		up->uartclk = ndata->new_rate * 2 * 1000;
+		termios.c_cflag = up->cons->cflag;
+		if (up->state->port.tty && termios.c_cflag == 0)
+			termios.c_cflag = up->state->port.tty->termios.c_cflag;
+		baud = uart_get_baud_rate(up, &termios, NULL,
+					  up->uartclk / 16 / UART_DIV_MAX,
+					  up->uartclk);
+		quot = uart_get_divisor(up, baud);
+
+		serial8250_rpm_get(uart);
+		spin_lock_irqsave(&up->lock, flags);
+		/* Update divisor*/
+		serial8250_do_set_divisor(up, baud, quot, 0);
+		/* Recover LCR state */
+		serial_out(uart, UART_LCR, uart->lcr);
+		/* Enable UART */
+		serial_out(uart, UART_BCM2835AUX_CNTL, 0x3);
+		spin_unlock_irqrestore(&up->lock, flags);
+		serial8250_rpm_put(uart);
+		console_unlock();
+
+		return NOTIFY_OK;
+	}
+	case ABORT_RATE_CHANGE:
+		serial8250_rpm_get(uart);
+		spin_lock_irqsave(&up->lock, flags);
+		/* Start UART */
+		serial_out(uart, UART_BCM2835AUX_CNTL, 0x3);
+		spin_unlock_irqrestore(&up->lock, flags);
+		serial8250_rpm_put(uart);
+		console_unlock();
+
+		return NOTIFY_OK;
+	default:
+		return NOTIFY_DONE;
+	}
+
+	return 0;
+}
 
 static int bcm2835aux_serial_probe(struct platform_device *pdev)
 {
@@ -102,6 +173,14 @@ static int bcm2835aux_serial_probe(struct platform_device *pdev)
 	}
 	data->line = ret;
 
+	data->clk_notifier.notifier_call = bcm2835aux_clk_notifier_cb;
+	ret = clk_notifier_register(data->clk, &data->clk_notifier);
+	if (ret) {
+		dev_err(&pdev->dev, "unable to register clk notifier - %d\n",
+			ret);
+		goto dis_clk;
+	}
+
 	platform_set_drvdata(pdev, data);
 
 	return 0;
@@ -116,6 +195,7 @@ static int bcm2835aux_serial_remove(struct platform_device *pdev)
 	struct bcm2835aux_data *data = platform_get_drvdata(pdev);
 
 	serial8250_unregister_port(data->uart.port.line);
+	clk_notifier_unregister(data->clk, &data->clk_notifier);
 	clk_disable_unprepare(data->clk);
 
 	return 0;
