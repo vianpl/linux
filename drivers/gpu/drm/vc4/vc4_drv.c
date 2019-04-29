b4244971897a37f8323ad10da64f622307bc4fd9
Nicolas Saenz Julienne <nsaenzjulienne@suse.de>
2019-04-29 12:05:35 +0200

    HACK: drm: vc4: bypass v3d as it's failing
    Signed-off-by: Nicolas Saenz Julienne <nsaenzjulienne@suse.de>

--- drivers/gpu/drm/vc4/vc4_drv.c
+++ drivers/gpu/drm/vc4/vc4_drv.c
@@ -330,7 +330,7 @@ static struct platform_driver *const component_drivers[] = {
 	&vc4_txp_driver,
 	&vc4_hvs_driver,
 	&vc4_crtc_driver,
-	&vc4_v3d_driver,
+	//&vc4_v3d_driver,
 };
 
 static int vc4_platform_drm_probe(struct platform_device *pdev)
