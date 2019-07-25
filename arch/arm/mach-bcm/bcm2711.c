03cc1c577392fe2effacca4e5858f995f2cce53f
Stefan Wahren <wahrenst@gmx.net>
2019-07-25 20:14:21 +0200

    ARM: bcm: Add support for BCM2711 SoC
    Add the BCM2711 to ARCH_BCM2835, but use new machine board code
    because of the differences.
    
    Signed-off-by: Stefan Wahren <wahrenst@gmx.net>

---
+++ arch/arm/mach-bcm/bcm2711.c
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2019 Stefan Wahren
+ */
+
+#include <linux/of_address.h>
+
+#include <asm/mach/arch.h>
+
+#include "platsmp.h"
+
+static const char * const bcm2711_compat[] = {
+#ifdef CONFIG_ARCH_MULTI_V7
+	"brcm,bcm2711",
+#endif
+};
+
+DT_MACHINE_START(BCM2711, "BCM2711")
+	.dma_zone_size	= SZ_1G,
+	.dt_compat = bcm2711_compat,
+	.smp = smp_ops(bcm2836_smp_ops),
+MACHINE_END
