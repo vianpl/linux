86cc90b0e8d1a278f17856c60ac5738cae1699da
Nicolas Saenz Julienne <nsaenzjulienne@suse.de>
2020-05-21 18:53:10 +0200

    staging: vchiq: Get rid of vchi
    All the functions that vchi currently provides are a 1:1 mapping to its
    vchiq counterparts. Get rid of vchi altogether and use vchiq's on all
    services.
    
    In the process also get rid of the vchi directory, as the only remaining
    file was a TODO file, which now lives in the parent directory.
    
    Signed-off-by: Nicolas Saenz Julienne <nsaenzjulienne@suse.de>

--- drivers/staging/vc04_services/interface/vchiq_arm/vchiq_arm.c
+++ drivers/staging/vc04_services/interface/vchiq_arm/vchiq_arm.c
@@ -2571,6 +2571,7 @@ vchiq_use_service(unsigned int handle)
 	}
 	return ret;
 }
+EXPORT_SYMBOL(vchiq_use_service);
 
 enum vchiq_status
 vchiq_release_service(unsigned int handle)
@@ -2584,6 +2585,7 @@ vchiq_release_service(unsigned int handle)
 	}
 	return ret;
 }
+EXPORT_SYMBOL(vchiq_release_service);
 
 struct service_data_struct {
 	int fourcc;
