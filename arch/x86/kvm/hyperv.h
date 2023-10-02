bf407dbc2d30c2fa5e965966bd2ad725b85234e6
Nicolas Saenz Julienne <nsaenz@amazon.com>
2023-10-02 15:19:16 +0000

    KVM: x86: hyper-v: Introduce KVM VTL device
    Introduce a new KVM device aimed at tracking partition wide VTL state,
    it'll be the one responsible from keeping track of VTL's memory
    protections. For now its functionality it's limited, it only exposes its
    VTL level through a device attribute. Additionally, the device type is
    only registered if the VSM cap is enabled.
    
    Signed-off-by: Nicolas Saenz Julienne <nsaenz@amazon.com>

--- arch/x86/kvm/hyperv.h
+++ arch/x86/kvm/hyperv.h
@@ -269,4 +269,7 @@ static inline void kvm_mmu_role_set_hv_bits(struct kvm_vcpu *vcpu,
 	role->vtl = kvm_hv_get_active_vtl(vcpu);
 }
 
+int kvm_hv_vtl_dev_register(void);
+void kvm_hv_vtl_dev_unregister(void);
+
 #endif
