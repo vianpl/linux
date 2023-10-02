190a1da77e4afa4c7c1a0c6696592146728907de
Nicolas Saenz Julienne <nsaenz@amazon.com>
2023-10-02 18:30:44 +0000

    KVM: Add KVM_EXIT_MEMORY_FAULT exit
    Signed-off-by: Nicolas Saenz Julienne <nsaenz@amazon.com>

--- include/uapi/linux/kvm.h
+++ include/uapi/linux/kvm.h
@@ -285,6 +285,7 @@ struct kvm_xen_exit {
 #define KVM_EXIT_RISCV_SBI        35
 #define KVM_EXIT_RISCV_CSR        36
 #define KVM_EXIT_NOTIFY           37
+#define KVM_EXIT_MEMORY_FAULT     38
 
 /* For KVM_EXIT_INTERNAL_ERROR */
 /* Emulate instruction failed. */
@@ -533,6 +534,12 @@ struct kvm_run {
 #define KVM_NOTIFY_CONTEXT_INVALID	(1 << 0)
 			__u32 flags;
 		} notify;
+		/* KVM_EXIT_MEMORY_FAULT */
+		struct {
+			__u64 flags;
+			__u64 gpa;
+			__u64 size;
+		} memory;
 		/* Fix the size of the union. */
 		char padding[256];
 	};
