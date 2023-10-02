35ea215a7754354146d1ed7c051f8738392184f1
Nicolas Saenz Julienne <nsaenz@amazon.com>
2023-10-02 18:25:28 +0000

    hyperv-vsm: introduce attrs ioctl
    Signed-off-by: Nicolas Saenz Julienne <nsaenz@amazon.com>

--- arch/x86/kvm/hyperv-vsm.c
+++ arch/x86/kvm/hyperv-vsm.c
@@ -9,6 +9,10 @@
 
 #include <linux/kvm_host.h>
 
+#define KVM_HV_VTL_ATTRS                                         \
+	(KVM_MEMORY_ATTRIBUTE_READ | KVM_MEMORY_ATTRIBUTE_WRITE | \
+		KVM_MEMORY_ATTRIBUTE_EXECUTE)
+
 struct kvm_hv_vtl_dev {
 	int vtl;
 	struct xarray mem_attrs;
@@ -39,14 +43,39 @@ static void kvm_hv_vtl_release(struct kvm_device *dev)
 	kfree(dev); /* alloc by kvm_ioctl_create_device, free by .release */
 }
 
-static int kvm_hv_vtl_create(struct kvm_device *dev, u32 type);
+static long kvm_hv_vtl_ioctl(struct kvm_device *dev, unsigned int ioctl,
+			     unsigned long arg)
+{
+	switch (ioctl) {
+	case KVM_SET_MEMORY_ATTRIBUTES: {
+		struct kvm_hv_vtl_dev *vtl_dev = dev->private;
+		struct kvm_memory_attributes attrs;
+		int r;
+
+		if (copy_from_user(&attrs, (void __user *)arg, sizeof(attrs)))
+			return -EFAULT;
+
+		r = kvm_set_mem_attributes(dev->kvm, &vtl_dev->mem_attrs,
+					   &attrs, KVM_HV_VTL_ATTRS);
+		if (r)
+			return r;
+
+		if (copy_to_user((void __user *)arg, &attrs, sizeof(attrs)))
+			return -EFAULT;
+		break;
+	}
+	default:
+		return -ENOTTY;
+	}
+
+	return 0;
+}
 
 static struct kvm_device_ops kvm_hv_vtl_ops = {
 	.name = "kvm-hv-vtl",
 	.create = kvm_hv_vtl_create,
 	.release = kvm_hv_vtl_release,
-	/* .ioctl = kvm_hv_vtl_ioctl, */
-	/* .set_attr = kvm_hv_vtl_set_attr, */
+	.ioctl = kvm_hv_vtl_ioctl,
 	.get_attr = kvm_hv_vtl_get_attr,
 };
 
