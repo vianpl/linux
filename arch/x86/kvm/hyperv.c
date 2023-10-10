ef99b575c5debb9511ba91a104dda7fad24460f8
Nicolas Saenz Julienne <nsaenz@amazon.com>
2023-10-10 14:20:25 +0000

    GET VSM STATE
    Signed-off-by: Nicolas Saenz Julienne <nsaenz@amazon.com>

--- arch/x86/kvm/hyperv.c
+++ arch/x86/kvm/hyperv.c
@@ -3118,3 +3118,21 @@ int kvm_get_hv_cpuid(struct kvm_vcpu *vcpu, struct kvm_cpuid2 *cpuid,
 
 	return 0;
 }
+
+int kvm_vm_ioctl_get_hv_vsm_state(struct kvm *kvm, struct kvm_hv_vsm_state *state)
+{
+	struct kvm_hv* hv = &kvm->arch.hyperv;
+
+	state->vsm_code_page_offsets64 = hv->vsm_code_page_offsets64.as_u64;
+	state->vsm_code_page_offsets32 = hv->vsm_code_page_offsets32.as_u64;
+	return 0;
+}
+
+int kvm_vm_ioctl_set_hv_vsm_state(struct kvm *kvm, struct kvm_hv_vsm_state *state)
+{
+	struct kvm_hv* hv = &kvm->arch.hyperv;
+
+	hv->vsm_code_page_offsets64.as_u64 = state->vsm_code_page_offsets64;
+	hv->vsm_code_page_offsets32.as_u64 = state->vsm_code_page_offsets32;
+	return 0;
+}
