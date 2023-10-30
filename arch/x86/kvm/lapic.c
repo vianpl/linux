e568c39e36564035c39d05706eb934cba860cff4
Nicolas Saenz Julienne <nsaenz@amazon.com>
2023-10-30 18:39:35 +0000

    KVM: x86: Don't use hv_timer if CAP_HYPERV_VSM enabled
    VSM's VTLs are modeled by using a distinct vCPU per VTL. While one VTL
    is running the rest of vCPUs are left idle. This doesn't play well with
    the approach of tracking emulated timer expiration by using the VMX
    preemption timer. Inactive VTL's timers are still meant to run and
    inject interrupts regardless of their runstate.
    
    Signed-off-by: Nicolas Saenz Julienne <nsaenz@amazon.com>

--- arch/x86/kvm/lapic.c
+++ arch/x86/kvm/lapic.c
@@ -152,9 +152,10 @@ static bool kvm_can_post_timer_interrupt(struct kvm_vcpu *vcpu)
 
 bool kvm_can_use_hv_timer(struct kvm_vcpu *vcpu)
 {
-	return kvm_x86_ops.set_hv_timer
-	       && !(kvm_mwait_in_guest(vcpu->kvm) ||
-		    kvm_can_post_timer_interrupt(vcpu));
+	return kvm_x86_ops.set_hv_timer &&
+	       !(kvm_mwait_in_guest(vcpu->kvm) ||
+		 kvm_can_post_timer_interrupt(vcpu)) &&
+	       !(kvm_hv_vsm_enabled(vcpu->kvm));
 }
 
 static bool kvm_use_posted_timer_interrupt(struct kvm_vcpu *vcpu)
