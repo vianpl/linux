9ff4db910af373c4285ae5bb50c7ffa2c46361cd
Nicolas Saenz Julienne <nsaenz@amazon.com>
2024-08-30 16:48:08 +0000

    KVM: Introduce a generation number for memory attributes
    Introduce a generation number to track memory attribute modifications.
    This will allow KVM components to invalidate any assumptions they might
    have about guest's physical addresses and their permissions when memory
    attributes change.
    
    Like with memory slot updates, it's mandatory for components that access
    guest memory based on cached information to do so within a KVM SRCU
    read-side critical section, and that they validate the generation number
    before accessing memory. This, in combination with the
    synchronize_srcu() call within the memory attributes ioctl handler,
    ensures the following:
    - A memory attribute modification operation only returns after all
      users of outdated GPA data are done running.
    - Any component accessing cached data after the memory attribute
      modification returned will see the updated generation number.
    
    Ultimately, since synchronize_srcu_expedited() is an expensive
    operation, only perform it when absolutely necessary. Do so if the
    introduced memory attribute is known to require synchronization or if
    the attribute being cleared contained a memory attribute that required
    synchronization.
    
    Signed-off-by: Nicolas Saenz Julienne <nsaenz@amazon.com>

--- include/trace/events/kvm.h
+++ include/trace/events/kvm.h
@@ -490,23 +490,26 @@ TRACE_EVENT(kvm_test_age_hva,
 );
 
 TRACE_EVENT(kvm_vm_set_mem_attributes,
-	TP_PROTO(u64 start, u64 cnt, u64 attributes),
-	TP_ARGS(start, cnt, attributes),
+	TP_PROTO(u64 start, u64 cnt, u64 attributes, bool sync),
+	TP_ARGS(start, cnt, attributes, sync),
 
 	TP_STRUCT__entry(
 		__field(	u64,	start		)
 		__field(	u64,	cnt		)
 		__field(	u64,	attributes	)
+		__field(	bool,	sync		)
 	),
 
 	TP_fast_assign(
 		__entry->start		= start;
 		__entry->cnt		= cnt;
 		__entry->attributes	= attributes;
+		__entry->sync		= sync;
 	),
 
-	TP_printk("gfn 0x%llx, cnt 0x%llx, attributes 0x%llx",
-		  __entry->start, __entry->cnt, __entry->attributes)
+	TP_printk("gfn 0x%llx, cnt 0x%llx, attributes 0x%llx, sync %d",
+		  __entry->start, __entry->cnt, __entry->attributes,
+		  __entry->sync)
 );
 
 #endif /* _TRACE_KVM_MAIN_H */
