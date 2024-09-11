171613c1eb69dffca3916cd438c69d2eb59781cf
Nicolas Saenz Julienne <nsaenz@amazon.com>
2024-09-11 18:25:31 +0000

    KVM: x86/mmu: Do not update accessed/dirty if guest PTE read-only
    Signed-off-by: Nicolas Saenz Julienne <nsaenz@amazon.com>

--- arch/x86/kvm/mmu/paging_tmpl.h
+++ arch/x86/kvm/mmu/paging_tmpl.h
@@ -409,6 +409,9 @@ retry_walk:
 			goto error;
 		}
 
+		if (!kvm_memory_attributes_write_allowed(vcpu->kvm, gpa_to_gfn(real_gpa)))
+			walker->pte_writable[walker->level - 1] = false;
+
 		ptep_user = (pt_element_t __user *)((void *)host_addr + offset);
 		if (unlikely(__get_user(pte, ptep_user)))
 			goto error;
