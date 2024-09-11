7dcfa91f739743b845f721eaf5530c265d290528
Nicolas Saenz Julienne <nsaenz@amazon.com>
2024-09-11 17:01:38 +0000

    KVM: selftests: Take into account mixed memory fault flags
    Update x86_64/private_mem_kvm_exits_test to take into account memory
    fault flags might contain multiple bits set while remaining valid. For
    example KVM_MEMORY_EXIT_FLAG_PRIVATE and KVM_MEMORY_EXIT_FLAG_READ.
    
    Signed-off-by: Nicolas Saenz Julienne <nsaenz@amazon.com>

--- tools/testing/selftests/kvm/x86_64/private_mem_kvm_exits_test.c
+++ tools/testing/selftests/kvm/x86_64/private_mem_kvm_exits_test.c
@@ -75,7 +75,8 @@ static void test_private_access_memslot_deleted(void)
 	exit_reason = (uint32_t)(uint64_t)thread_return;
 
 	TEST_ASSERT_EQ(exit_reason, KVM_EXIT_MEMORY_FAULT);
-	TEST_ASSERT_EQ(vcpu->run->memory_fault.flags, KVM_MEMORY_EXIT_FLAG_PRIVATE);
+	TEST_ASSERT(vcpu->run->memory_fault.flags & KVM_MEMORY_EXIT_FLAG_PRIVATE,
+		    "Memory fault didn't occur on a private memory access");
 	TEST_ASSERT_EQ(vcpu->run->memory_fault.gpa, EXITS_TEST_GPA);
 	TEST_ASSERT_EQ(vcpu->run->memory_fault.size, EXITS_TEST_SIZE);
 
@@ -104,7 +105,8 @@ static void test_private_access_memslot_not_private(void)
 	exit_reason = run_vcpu_get_exit_reason(vcpu);
 
 	TEST_ASSERT_EQ(exit_reason, KVM_EXIT_MEMORY_FAULT);
-	TEST_ASSERT_EQ(vcpu->run->memory_fault.flags, KVM_MEMORY_EXIT_FLAG_PRIVATE);
+	TEST_ASSERT(vcpu->run->memory_fault.flags & KVM_MEMORY_EXIT_FLAG_PRIVATE,
+		    "Memory fault didn't occur on a private memory access");
 	TEST_ASSERT_EQ(vcpu->run->memory_fault.gpa, EXITS_TEST_GPA);
 	TEST_ASSERT_EQ(vcpu->run->memory_fault.size, EXITS_TEST_SIZE);
 
