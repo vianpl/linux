a452e3f07f83d604d438d06968b6357650155e92
Nicolas Saenz Julienne <nsaenzju@redhat.com>
2025-03-26 20:31:09 +0100

    Get rid of selftest haxk
    Signed-off-by: Nicolas Saenz Julienne <nsaenzju@redhat.com>

--- tools/testing/selftests/kvm/lib/kvm_util.c
+++ tools/testing/selftests/kvm/lib/kvm_util.c
@@ -16,7 +16,6 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <linux/kernel.h>
-#include <linux/memfd.h>
 
 #define KVM_UTIL_MIN_PFN	2
 
@@ -35,10 +34,6 @@ int open_path_or_exit(const char *path, int flags)
 	TEST_ASSERT(fd >= 0, "Failed to open '%s'", path);
 
 	return fd;
-}
-
-static inline int memfd_create(const char *name, unsigned int flags) {
-    return syscall(SYS_memfd_create, name, flags);
 }
 
 /*
